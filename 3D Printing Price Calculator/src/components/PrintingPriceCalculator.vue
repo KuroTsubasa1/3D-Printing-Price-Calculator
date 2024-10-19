d0626b59d3260194b0b88a6a514c16bc50080d20
Lasse Harm <1-IBI@web.de>
2024-10-19 21:29:45 +0200

    Update grid layout and add margins to improve space utilization and reduce cramped feeling
    * Implement a grid layout with responsive columns
    * Add margins to both sides of the page to reduce cramped feeling

--- 3D Printing Price Calculator/src/components/PrintingPriceCalculator.vue
+++ 3D Printing Price Calculator/src/components/PrintingPriceCalculator.vue
@@ -108,7 +108,7 @@ onUnmounted(() => {
 </script>
 
 <template>
-  <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 p-4">
+  <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 p-4 mx-4">
 
     <div class="col-span-1 md:col-span-2 lg:col-span-3 text-center">
       <h1 class="font-sans text-4xl font-bold mb-5">3D Printing Price Calculator</h1>
